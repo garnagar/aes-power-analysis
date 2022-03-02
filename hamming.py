ccfee38be2f9ba390ac445a750c83f7c3c432e62
William Wulff <william.wulff@gmail.com>
2022-03-02 16:34:25 +0100

    Added hamming weight function

---
+++ hamming.py
@@ -0,0 +1,11 @@
+# Module hamming.py
+import numpy as np
+
+def hammingDistance(x):
+    count = 0
+    for i in range(8):
+        shifted = np.right_shift(x, i)
+        if np.bitwise_and(shifted, 1 == 1):
+            count = count + 1
+
+    return count
