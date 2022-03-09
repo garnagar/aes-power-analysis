4452cd50c398963d93bb1960368a6994d7324707
Lukas Kyzlik <lukas.kyzlik@gmail.com>
2022-03-09 15:07:24 +0100

    correlation fixed

--- corr.py
+++ corr.py
@@ -2,14 +2,14 @@ import numpy as np
 
 def calcCorrelationMatrix(T, H):
     # Prepare variables
-    samplesCount = T.shape[0]
-    keyCount = H.shape[0]
-    assert T.shape[1] == H.shape[1]  # Check if the number of plaintexts is the same
-    C = np.zeros([samplesCount, keyCount])
+    samplesCount = T.shape[1]
+    keyCount = H.shape[1]
+    assert T.shape[0] == H.shape[0]  # Check if the number of plaintexts is the same
+    C = np.zeros([keyCount, samplesCount])
 
     # Calculate correlation
     for i in range(keyCount):
         for j in range(samplesCount):
-            C[i, j] = np.corrcoef(H[:, i].T, T[:, j].T)
+            C[i, j] = np.corrcoef(H[:, i].T, T[:, j].T)[0, 1]
 
     return C
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from load import loadFloats, loadInts
 from simTraces import generateSimTrace
+from corr import calcCorrelationMatrix
 
 def main():
-    print(generateSimTrace("data/inputs1.dat"))
+    T = loadFloats("data/T1.dat")
+    H = generateSimTrace("data/inputs1.dat")
+    C = calcCorrelationMatrix(T, H)
+    print(C)
 
 if __name__ == '__main__':
     main()
