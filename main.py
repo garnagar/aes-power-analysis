4e6c322a06e8605d5e8158166ce1ea25be1f641c
William Wulff <william.wulff@gmail.com>
2022-03-09 15:51:19 +0100

    Added graphical test for provided testing inputs

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from load import loadFloats, loadInts
 from simTraces import generateSimTrace
 from corr import calcCorrelationMatrix
+import matplotlib.pyplot as plt
 
 def main():
     T = loadFloats("data/T1.dat")
     H = generateSimTrace("data/inputs1.dat")
     C = calcCorrelationMatrix(T, H)
     print(C)
+
+    # Graphic test to show correlation for
+    # test dataset (correct keybyte = 203 DEC)
+    T = loadFloats("data/T_test.dat")
+    H = generateSimTrace("data/inputs_test.dat")
+    C = calcCorrelationMatrix(T, H)
+    
+    plt.plot(C[0,:], label='Keybyte 0')
+    plt.plot(C[50,:], label='Keybyte 50')
+    plt.plot(C[203,:], label='Keybyte 203')
+
+    plt.legend()
+    plt.show()
 
 if __name__ == '__main__':
     main()
